c0e032d1cddfb92389aba6fa45ee471db4b5e566
MatheusLobo <MatheusLobo@DESKTOP-IPO0OTO>
2023-05-12 05:47:20 -0300

    Signed-off-by: MatheusLobo <MatheusLobo@DESKTOP-IPO0OTO>

--- beeCrowd/src/beeCrowd/NumeroPerfeito_1164.java
+++ beeCrowd/src/beeCrowd/NumeroPerfeito_1164.java
@@ -3,33 +3,24 @@ package beeCrowd;
 import java.util.Scanner;
 
 public class NumeroPerfeito_1164 {
-
-	public static void main(String[] args) {
-		int qnt,num,aux=0,div,aux2;;
-		Scanner read = new Scanner (System.in);
-		qnt= read.nextInt();
-		while (aux<qnt) {
-			num=read.nextInt();
-			div=0;
-			for (int i=1; i<num;i++) {
-				if (num % i == 0) {
+    public static void main(String[] args) {
+        int qnt, num, aux = 0, div = 0;
+        Scanner read = new Scanner(System.in);
+        qnt = read.nextInt();
+        while (aux < qnt) {
+            num = read.nextInt();
+            for (int i = 1; i < num; i++) {
+                if (num % i == 0) {
                     div += i;
                 }
-				if (div==num) {
-					System.out.println(num+" Pefeito");
-					break;
-				} else if (div>num) {
-					System.out.println(num+" nao eh perfeito");
-				}
-				
-				}
-			aux++;
-			if (aux==qnt) {
-				break;
-			}
-		
-			}
-		}
-	}
-
-
+            }
+            if (div == num) {
+                System.out.println(num + " eh perfeito");
+            } else {
+                System.out.println(num + " nao eh perfeito");
+            }
+            aux++;
+            div = 0;
+        }
+    }
+}
