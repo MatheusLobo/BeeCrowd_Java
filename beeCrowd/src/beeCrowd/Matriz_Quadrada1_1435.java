9692bc8563454077fcc94ea6a317276a06088fd3
MatheusLobo <mat_lo@hotmail.com>
2023-07-11 11:41:52 -0300

    1
    1

--- beeCrowd/src/beeCrowd/Matriz_Quadrada1_1435.java
+++ beeCrowd/src/beeCrowd/Matriz_Quadrada1_1435.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class Matriz_Quadrada1_1435 {
 	
 	public static void main(String[] args) {
-		int x;
+		int x=0;
 		Scanner read = new Scanner (System.in);
-		while(true) {
+		while(0<=x && x<=100) {
 			x=read.nextInt();
-			while (x>0) {
-			System.out.println(x);
-			x--;
+			while (x>=0) {
+				System.out.println(x);
+				x=x-1;
+				
 			}
 			
 		}
